38cf475d4db5941c13af54b7c2a0602c9aa69d36
BeJian <110755480+BeJian@users.noreply.github.com>
2026-05-24 14:55:09 +0800

    1

--- SCFM.py
+++ SCFM.py
@@ -114,7 +114,7 @@ def scfm(datax, datay, testx, testy, dataset):
         raise ValueError("No available grid_size")
     print('final grid_size:', grid_size)
 
-    output_dir = 'dataset/FAG_images_'+str(dataset)
+    output_dir = 'dataset/images_'+str(dataset)
     os.makedirs(output_dir, exist_ok=True)
 
     # save image
@@ -159,4 +159,4 @@ def scfm(datax, datay, testx, testy, dataset):
 
 X, Y = read_raw_data('dataset/RP_1043/tabular/Train/chiller_L1_Train_10')
 testX, testY = read_raw_data('dataset/RP_1043/tabular/Test/chiller_L1_Test_300')
-scfm(X, Y, X, Y, 'chiller_image')
+scfm(X, Y, X, Y, 'chiller_10')
